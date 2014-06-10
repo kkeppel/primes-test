aea065082ab649ddc0ace6233154ea8b8a13f1fa
Kathy Keppel <kathykeppel@gmail.com>
2014-06-10 14:30:03 -0400

    make input default to 10 primes

--- prime_test.rb
+++ prime_test.rb
@@ -9,7 +9,7 @@ class PrimeTest
 
   def self.get_n_prime_numbers
     puts "Enter number of primes:"
-    primes_count = PrimeTest.get_n || 10
+    primes_count = PrimeTest.get_n == "" ? 10 : PrimeTest.gets_n
 
     primes_count = primes_count.to_i
     primes = [2]
